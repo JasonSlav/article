faafcad56347c42dc445a15d1e1e82a6b7031034
JasonSlav <joyapus257@gmail.com>
2023-12-15 22:19:56 +0700

    nganyari file

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,14 @@
-import type { MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+import { useState } from "react";
 
 export default function Index() {
+  const [number, setNumber] = useState(0);
+  const addNumber = () => {
+    setNumber(number + 1);
+  };
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <h1>{number}</h1>
+      <button onClick={addNumber}>Tambah Angka</button>
+    </>
   );
 }
