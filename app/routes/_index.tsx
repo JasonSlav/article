75f0a9614dfea86a058ee11e83f8b928ea00c5aa
lukiznr <lukyfriendly@gmail.com>
2023-12-15 15:53:11 +0000

    new

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
 export default function Index() {
-  const [number, setNumber] = useState(0);
+  const [number, setNumber] = useState(999);
   const addNumber = () => {
     setNumber(number + 1);
   };
   return (
     <>
-      <h1>{number}</h1>
+      <h1 clasName="text-8xl text-center">{number}</h1>
       <button onClick={addNumber}>Tambah Angka</button>
     </>
   );
