f31d8593cb83637a40e29128c28dcf13e6b31d0d
lukiznr <lukyfriendly@gmail.com>
2023-12-16 03:34:48 +0000

    new

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
-
+import {Theme, useTheme } from "remix-themes"
 export default function Index() {
   const [number, setNumber] = useState(999);
+  const [,setTheme] = useTheme()
   const addNumber = () => {
     setNumber(number + 1);
   };
   return (
     <>
       <h1 className="text-8xl text-center p-3">Title web</h1>
-      <h2 onClick={addNumber} className="bg-rosewater text-4xl text-center p-2">{number}</h2>
+      <h2 onClick={addNumber} className="bg-rosewater text-4xl text-center text-base p-2">{number}</h2>
+      <button
+        type="button"
+        onClick={() =>
+          setTheme(prev => (prev === Theme.DARK ? Theme.LIGHT : Theme.DARK))
+        }
+        className="bg-crust p-2 rounded-md"
+      >
+        Toggle theme
+      </button>
     </>
   );
 }
